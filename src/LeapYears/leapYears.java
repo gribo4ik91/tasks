ea1b92d1cd0d6b1a1b07e85a587802f26bb5a3bc
Andrei Grib <gribo4ik@gmail.com>
2020-09-17 17:56:57 +0300

    update

--- src/LeapYears/leapYears.java
+++ src/LeapYears/leapYears.java
@@ -3,6 +3,11 @@ package LeapYears;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//11
+//"Write a program that outputs the next k leap years
+//        https://en.wikipedia.org/wiki/Leap_year"
+
+
 public class leapYears {
 
     public static void main(String[] args) {
